392de90003e2842e2631d236d00205caf5ba0d37
Tip ten Brink <75669206+tiptenbrink@users.noreply.github.com>
2021-12-08 11:57:34 +0100

    dodeka->DSAV-Dodeka

--- build/build_db.sh
+++ build/build_db.sh
@@ -4,5 +4,5 @@
 poetry run python -c "from spawn_db import spawn_build; spawn_build('configged')"
 # metaconfig
 # db/configged should correspond to the directory of the built config
-docker build --tag ghcr.io/dodeka/postgres db/configged
+docker build --tag 'ghcr.io/DSAV-Dodeka/postgres' db/configged
 poetry run python -c "from spawn_db import spawn_deploy; spawn_deploy()"
