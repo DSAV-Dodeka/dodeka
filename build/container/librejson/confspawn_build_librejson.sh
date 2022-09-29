3e352eae5fcbf4fd10f7157eea9eb1febb279b05
Tip ten Brink <75669206+tiptenbrink@users.noreply.github.com>
2022-09-30 00:09:57 +0200

    Fix librejson build

--- build/container/librejson/confspawn_build_librejson.sh
+++ build/container/librejson/confspawn_build_librejson.sh
@@ -2,14 +2,14 @@
 # Exit when a command fails
 set -e
 # Download release archive
-gh release --repo RedisJSON/RedisJSON download {{ kv.redisjson_version }} --archive tar.gz --dir context/librejson
-tar -xf context/librejson/*RedisJSON*.tar.gz -C context/librejson
+gh release --repo RedisJSON/RedisJSON download {{ kv.redisjson_version }} --archive tar.gz --dir contextlibrejson
+tar -xf contextlibrejson/*RedisJSON*.tar.gz -C contextlibrejson
 # Remove archive so only dir will be copied
-rm context/librejson/*RedisJSON*.tar.gz
+rm contextlibrejson/*RedisJSON*.tar.gz
 # Copy Dockerfile to extracted dir
-cp context/librejson/Dockerfile context/librejson/*RedisJSON*
+cp contextlibrejson/Dockerfile contextlibrejson/*RedisJSON*
 # Build the Dockerfile which will build the librejson.so
-docker build --tag rejsonbuild context/librejson/*RedisJSON*
+docker build --tag rejsonbuild contextlibrejson/*RedisJSON*
 # Run container so it can be extracted
 docker run --name rejsonbuilder rejsonbuild
 docker cp rejsonbuilder:/build/RedisJSON/target/release/librejson.so context/librejson.so
