7abbe04330f8e5c41b6836533695db62cd5d560f
Tip ten Brink <75669206+tiptenbrink@users.noreply.github.com>
2022-10-01 12:18:17 +0200

    Super nice script to run deployment including passphrase locally

--- pull/pull.py
+++ pull/pull.py
@@ -3,27 +3,26 @@ from pathlib import Path
 import shutil
 
 
-def env():
-    env_str = input("Environment: ")
-
-    print(env_str)
-
-
 def passphrase():
     pw = getpass.getpass("Passphrase for deployment: ")
 
     print(pw)
 
 
-def move(time: str, env: str):
+def move(time: str, env_str: str):
     # Move deploy target to env folder for history
-    use_src_path = Path(f"./use/{env}")
-    deploy_target_path = Path(f"./deployments/{env}/deploy{time}")
+    print("Moving deployment scripts...")
+    use_src_path = Path(f"./use/{env_str}")
+    deploy_target_path = Path(f"./deployments/{env_str}/deploy{time}")
     deploy_target_path.parent.mkdir(parents=True, exist_ok=True)
     shutil.copytree(use_src_path, deploy_target_path)
     # Move deploy target to active
-    target_path = Path(f"./deployments/active{env}")
+    target_path = Path(f"./deployments/active{env_str}")
+    if target_path.exists():
+        shutil.rmtree(target_path)
     shutil.copytree(use_src_path, target_path)
     # Move secrets into active
-    secr_src_path = Path(f"./deployments/{env}/secrets{time}")
-    shutil.move(secr_src_path, target_path)
+    print("Moving secrets...")
+    secr_src_path = Path(f"./deployments/{env_str}/secrets{time}")
+    secr_target_path = target_path.joinpath("secrets")
+    shutil.copytree(secr_src_path, secr_target_path)
--- test.sh
+++ test.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
+echo "Setting up deployment in $ENV_STR mode..."
 set -e
+cd "${0%/*}" || exit
+CUR_DIR=$(pwd -P)
 PASSPHRASE=$(python3 -c 'import pull as p; p.passphrase()')
-ENV=$(python3 -c 'import pull as p; p.env()')
+ENV_STR="$1"
 DEPLOYID="$(date +%s)"
-gh repo clone DSAV-Dodeka/secrets ./secrets"$DEPLOYID"
-python3 -c "import pull as p; p.move(${DEPLOYID}, ${ENV})"
-
-echo "hi"
+echo "Current time: $DEPLOYID"
+SECRET_TARGET="./deployments/${ENV_STR}/secrets${DEPLOYID}"
+gh repo clone DSAV-Dodeka/secrets "$SECRET_TARGET"
+rm -rf "$SECRET_TARGET/.git"
+python3 -c "import pull as p; p.move('${DEPLOYID}', '${ENV_STR}')"
+cd "$SECRET_TARGET"
+echo "Decrypting..."
+set -a
+. decrypt.sh secretdb.env.gpg "$PASSPHRASE" || exit 1
+. decrypt.sh secretserver.env.gpg "$PASSPHRASE" || exit 1
+# TODO check existence?
+cd "$CUR_DIR/deployments/active$ENV_STR"
+echo "Running..."
+"./${ENV_STR}deploy.sh"
