0c1e9b955222b2b9c7a621f1675826aff65fe9d1
Tip ten Brink <75669206+tiptenbrink@users.noreply.github.com>
2022-10-02 15:01:39 +0200

    Restaging works properly now

--- build/deploy/staging/confspawn_stagingdeploy.sh
+++ build/deploy/staging/confspawn_stagingdeploy.sh
@@ -3,16 +3,20 @@ cd "${0%/*}" || exit
 CUR_DIR=$(pwd -P)
 set -a
 
+echo "Running staging deploy script with argument $1"
+
 if [ "$1" = "update" ]
 then
-  export RECREATE=no
   ./db/deploy.sh "$CUR_DIR"/staging.env || exit
+  ./kv/deploy.sh "$CUR_DIR"/staging.env || exit
+  ./server/deploy.sh "$CUR_DIR"/staging.env || exit
 else
+  echo "Shutting down and recreating database..."
   # Everything must be down for recreation and removing volume
   ./down.sh
   # 'rv' indicates remove volume, so database is started from scratch
-  ./db/deploy.sh "$CUR_DIR"/staging.env rv || exit
+  ./db/deploy.sh "$CUR_DIR"/restaging.env rv || exit
+  ./kv/deploy.sh "$CUR_DIR"/restaging.env || exit
+  ./server/deploy.sh "$CUR_DIR"/restaging.env || exit
 fi
 
-./kv/deploy.sh "$CUR_DIR"/staging.env || exit
-./server/deploy.sh "$CUR_DIR"/staging.env || exit
