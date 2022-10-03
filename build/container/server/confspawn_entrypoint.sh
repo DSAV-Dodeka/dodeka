914403cd38c845a33541cc255618f4500c7bc4c1
Tip ten Brink <75669206+tiptenbrink@users.noreply.github.com>
2022-10-03 17:47:27 +0200

    Update server

--- build/container/server/confspawn_entrypoint.sh
+++ build/container/server/confspawn_entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export GUNICORN_CMD_ARGS="--bind=0.0.0.0:{{ server.container_port }} --workers=3"
-/dodeka/server/bin/gunicorn apiserver.app:app -w 3 -k uvicorn.workers.UvicornWorker
+/dodeka/server/bin/gunicorn apiserver.app:apiserver_app -w 3 -k uvicorn.workers.UvicornWorker
