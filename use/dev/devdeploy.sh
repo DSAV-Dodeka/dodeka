6cb595b6622e674df62779e24a222a0f6313bcac
Tip ten Brink <75669206+tiptenbrink@users.noreply.github.com>
2023-03-21 12:13:12 +0100

    Add 'port' option to devdeploy for WSL

--- use/dev/devdeploy.sh
+++ use/dev/devdeploy.sh
@@ -4,13 +4,22 @@ CUR_DIR=$(pwd -P)
 set -a
 
 # Load dev environment variables from dev.env file
-echo "$CUR_DIR"/dev.env
+# The 'port' option indicates to use the dev_env with the changed port
+# This is necessary on WSL where you cannot expose to just localhost
+if [ "$1" = "port" ] || [ "$2" = "port" ]
+then
+  DEV_ENV="$CUR_DIR"/dev_port.env
+else
+  DEV_ENV="$CUR_DIR"/dev.env
+fi
+
+echo "$DEV_ENV"
 # Start database, then key-value store
-./db/deploy.sh "$CUR_DIR"/dev.env || exit
-./kv/deploy.sh "$CUR_DIR"/dev.env || exit
+./db/deploy.sh "$DEV_ENV" || exit
+./kv/deploy.sh "$DEV_ENV" || exit
 
 # By default, the server is not started as it is generally run from Python in development
 if [ "$1" = "server" ]
 then
-  ./server/deploy.sh "$CUR_DIR"/dev.env || exit
+  ./server/deploy.sh "$DEV_ENV" || exit
 fi
