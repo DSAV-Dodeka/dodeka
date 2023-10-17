0975d381135fe41c386b2e42c3ec758143df1f7e
Tip ten Brink <75669206+tiptenbrink@users.noreply.github.com>
2023-10-17 13:24:26 +0200

    Alembic and migrate download done

--- build/data_sync/migrate.py
+++
@@ -1,7 +0,0 @@
-from dirgh import find_download
-from dirgh.cli_helper import find_token
-
-
-def download_migration_env(version: str = "v0.2.0"):
-    token = find_token("dodeka_", True)
-    find_download("DSAV-Dodeka", "backend", "src/schema", "migrate", ref=f"tags/{version}", token=token, overwrite=True)
