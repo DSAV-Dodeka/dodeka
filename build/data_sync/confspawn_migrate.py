083b314e393d3b59002aaa2ea5f709b0e9cd9557
Tip ten Brink <75669206+tiptenbrink@users.noreply.github.com>
2023-10-17 14:08:25 +0200

    Migrate add connection .json

--- build/data_sync/confspawn_migrate.py
+++ build/data_sync/confspawn_migrate.py
@@ -3,7 +3,7 @@ from dirgh import find_download, run_sync
 from dirgh.cli_helper import find_token
 
 
-def download_migration_env(version: str = "v2.0.0"):
+def download_migration_env(version: str = "HEAD"):
     token = find_token("dodeka_", True)
 
     download = partial(find_download,
@@ -11,7 +11,7 @@ def download_migration_env(version: str = "v2.0.0"):
                        "backend",
                        "src/schema",
                        "migrate",
-                       ref=f"tags/{version}",
+                       ref=f"{version}",
                        token=token,
                        overwrite=True)
 
