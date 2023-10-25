b0ca0ac00c3a3283364fca9a1df6ec71b0030967
Tip ten Brink <75669206+tiptenbrink@users.noreply.github.com>
2023-10-25 10:55:17 +0200

    v2.0.1

---
+++ build/data_sync/confspawn_migrate_env.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+poetry run python -c "from use.data_sync.migrate import download_migration_env; download_migration_env('{{ confspawn_env.version }}')" || exit 1
+cp use/data_sync/connect.json migrate/connect.json
--- use/data_sync/migrate_env.sh
+++ use/data_sync/migrate_env.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-poetry run python -c "from use.data_sync.migrate import download_migration_env; download_migration_env()" || exit 1
+poetry run python -c "from use.data_sync.migrate import download_migration_env; download_migration_env('v2.0.1')" || exit 1
 cp use/data_sync/connect.json migrate/connect.json
