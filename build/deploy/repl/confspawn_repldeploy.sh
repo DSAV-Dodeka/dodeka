782cb390cbbc14ad4355c4f38a5a101f5b806a50
Tip ten Brink <75669206+tiptenbrink@users.noreply.github.com>
2023-11-13 20:57:19 +0100

    significantly simplify deployment->into 1 compose

--- build/deploy/repl/confspawn_repldeploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd "${0%/*}" || exit
-CUR_DIR=$(pwd -P)
-set -a
-
-echo "Running repl deploy script with argument $1"
-
-./db/deploy.sh "$CUR_DIR"/repl.env || exit
--- build/deploy/repl/down.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cd "${0%/*}" || exit
-# This ensures all env variables are exported so env variables used in deploy.env (like $HOME) are
-# properly expanded
-# env files are consumed by e.g. docker compose
-set -a
-# Load environment variables from repl.env
-. ./repl.env
-./db/down.sh
