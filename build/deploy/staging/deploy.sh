e3c3daea96111fd0e52a6d8fa4f05d97b55b8b0c
Tip ten Brink <75669206+tiptenbrink@users.noreply.github.com>
2023-11-14 00:05:59 +0100

    staging recreate working again

--- build/deploy/staging/deploy.sh
+++ build/deploy/staging/deploy.sh
@@ -1,2 +1,10 @@
+# by default it will recreate the database
+if [ "$1" = "update" ]
+then
+  export RECREATE=no
+else
+  export RECREATE=yes
+fi
+
 docker compose --env-file staging.env --profile all pull
 docker compose --env-file staging.env --profile all up -d
