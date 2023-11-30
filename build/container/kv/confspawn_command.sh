dcb849b5aef7e6f72b95b32559a23cb36d2ae077
Tip ten Brink <75669206+tiptenbrink@users.noreply.github.com>
2023-11-30 15:48:54 +0100

    fix host and dont save kv

--- build/container/kv/confspawn_command.sh
+++ build/container/kv/confspawn_command.sh
@@ -1,7 +1,8 @@
 #/bin/sh
 
-# Check if the $HI variable is set
+# Check if the $REDIS_PASSWORD variable is set
 if [ -n "$REDIS_PASSWORD" ]; then
+  echo "Starting redis...."
   redis-server {{ kv.redis_conf_dir }} --requirepass "$REDIS_PASSWORD"
 else
   echo "Error: \$REDIS_PASSWORD must be set!"
