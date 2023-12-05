5d0f6de6ef268274b6a352446aaac9c130234766
Tip ten Brink <75669206+tiptenbrink@users.noreply.github.com>
2023-12-05 16:25:36 +0100

    fix localdev and rebuild

--- use/data_sync/migrate_env.sh
+++ use/data_sync/migrate_env.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-poetry run python -c "from use.data_sync.migrate import download_migration_env; download_migration_env('v2.1.0')" || exit 1
+poetry run python -c "from use.data_sync.migrate import download_migration_env; download_migration_env('v2.1.1')" || exit 1
 cp use/data_sync/connect.json migrate/connect.json
