db73b7c9d84fa3128c7c8210ec76e0a41910df93
Tip ten Brink <75669206+tiptenbrink@users.noreply.github.com>
2023-12-05 14:54:00 +0100

    fix version vars

--- use/staging/entrypoint.sh
+++ use/staging/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 export COMPOSE_PROJECT_NAME="dodeka-${DEPLOY_NAME}"
 export NETWORK_NAME="dodeka-${DEPLOY_NAME}"
-export KV_VERSION=staging-${DEPLOY_TAG}
-export KDB_VERSION=staging-${DEPLOY_TAG}
-export KSERVER_VERSION=staging-${DEPLOY_TAG}
+export KV_VERSION=staging${DEPLOY_TAG_SUFFIX}
+export DB_VERSION=staging${DEPLOY_TAG_SUFFIX}
+export SERVER_VERSION=staging${DEPLOY_TAG_SUFFIX}
 docker compose --env-file staging.env --profile all pull
 docker compose --env-file staging.env --profile all up -d
