0d6ce0c70606a2082923ea27f189a67744b6de2f
Tip ten Brink <75669206+tiptenbrink@users.noreply.github.com>
2023-12-05 14:39:48 +0100

    staging can pick correct version

--- build/deploy/production/confspawn_entrypoint.sh
+++ build/deploy/production/confspawn_entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export COMPOSE_PROJECT_NAME="{{ main.project }}${DEPLOY_NAME}"
-export NETWORK_NAME="{{ main.docker_net_name }}${DEPLOY_NAME}"
-docker compose --profile all pull
-docker compose --profile all up -d
+export COMPOSE_PROJECT_NAME="{{ main.project }}-${DEPLOY_NAME}"
+export NETWORK_NAME="{{ main.docker_net_name }}-${DEPLOY_NAME}"
+docker compose --env-file production.env --profile all pull
+docker compose --env-file production.env --profile all up -d
