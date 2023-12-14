bf711114b7fdaf9a586ab8744664559592ebbae2
Tip ten Brink <75669206+tiptenbrink@users.noreply.github.com>
2023-12-14 13:59:01 +0100

    update staging for new tidploy

--- deploy/build/deploy/staging/source.sh
+++ deploy/build/deploy/staging/source.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
+# we want to auto-export all environment variables we set so docker compose can use them
 set -a
 echo "Waiting for secrets..."
 while [ true ] 
 do 
     # if file exists and is named pipe
     if [ -p "$1" ]; then
-        echo "Loaded secret."
         . $1
+        if [ -n "$TIDPLOY_READY" ]; then
+            echo "Starting...."
+            ./entrypoint.sh
+            break
+        else
+            echo "Secrets loaded."
+        fi
+    # if pipe doesn't exist we don't want to run too many loops
     else
         sleep 1
-    fi
-
-    if [ -n "$TIDPLOY_READY" ]; then
-        echo "Starting...."
-        ./entrypoint.sh
-        break
     fi
 done
