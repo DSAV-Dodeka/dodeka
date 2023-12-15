7abe5b31d0838431b986821e07bc5289ee435ed6
Tip ten Brink <75669206+tiptenbrink@users.noreply.github.com>
2023-12-15 21:04:39 +0100

    more changes for tidploy 0.11

--- deploy/build/deploy/staging/confspawn_entrypoint.sh
+++ deploy/build/deploy/staging/confspawn_entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-export COMPOSE_PROJECT_NAME="{{ main.project }}-${DEPLOY_NAME}"
-export NETWORK_NAME="{{ main.docker_net_name }}-${DEPLOY_NAME}"
-export KV_VERSION=staging${DEPLOY_TAG_SUFFIX}
-export DB_VERSION=staging${DEPLOY_TAG_SUFFIX}
-export SERVER_VERSION=staging${DEPLOY_TAG_SUFFIX}
+export COMPOSE_PROJECT_NAME="{{ main.project }}-${TIDPLOY_TAG}"
+export NETWORK_NAME="{{ main.docker_net_name }}-${TIDPLOY_TAG}"
+export KV_VERSION=staging-${TIDPLOY_SHA}
+export DB_VERSION=staging-${TIDPLOY_SHA}
+export SERVER_VERSION=staging-${TIDPLOY_SHA}
 docker compose --env-file staging.env --profile all pull
 docker compose --env-file staging.env --profile all up -d
