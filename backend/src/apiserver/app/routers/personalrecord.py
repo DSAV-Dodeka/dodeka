9ea21ef649400ac5152bde4128e6ad498f9bf1db
Leander Bindt <leander.bindt@ziggo.nl>
2024-01-04 05:05:31 +0100

    Huts update 2

--- backend/src/apiserver/app/routers/personalrecord.py
+++ backend/src/apiserver/app/routers/personalrecord.py
@@ -12,6 +12,6 @@ personal_record_router = APIRouter(
 async def read_root() -> dict[str, str]:
     return {"PR": "Die heb jij niet loser, womp womp"}
 
-@personal_record_router.get("/get/{onderdeel}", response_model=dict[str, str])
-async def get_onderdeel(onderdeel: str) -> RawJSONResponse:
-    return {"Onderdeel": onderdeel}
+@personal_record_router.get("/get/", response_model=dict[str, str])
+async def get_onderdeel() -> RawJSONResponse:
+    return {"Onderdeel": "allemaal"}
