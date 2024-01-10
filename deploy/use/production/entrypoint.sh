358b759f72521e46088c85210364ef276f112e8a
Tip ten Brink <75669206+tiptenbrink@users.noreply.github.com>
2024-01-10 23:43:42 +0100

    fix image version for production

--- deploy/use/production/entrypoint.sh
+++ deploy/use/production/entrypoint.sh
@@ -2,8 +2,8 @@
 export DEPLOY_NAME="production-${TIDPLOY_TAG}"
 export COMPOSE_PROJECT_NAME="dodeka-${DEPLOY_NAME}"
 export NETWORK_NAME="dodeka-${DEPLOY_NAME}"
-export KV_VERSION="production-${TIDPLOY_SHA}"
-export DB_VERSION="production-${TIDPLOY_SHA}"
-export SERVER_VERSION="production-${TIDPLOY_SHA}"
+export KV_VERSION="${TIDPLOY_SHA}"
+export DB_VERSION="${TIDPLOY_SHA}"
+export SERVER_VERSION="${TIDPLOY_SHA}"
 docker compose --env-file production.env --profile all pull
 docker compose --env-file production.env --profile all up -d
