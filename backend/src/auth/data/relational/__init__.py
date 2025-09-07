2a22232377c7dc6511029a7423c24d9b5c6bdd08
Tip ten Brink <75669206+tiptenbrink@users.noreply.github.com>
2025-09-07 19:23:17 +0200

    prepare for v4: deps and simpler structure and delete

--- backend/src/auth/data/relational/__init__.py
+++
@@ -1,2 +0,0 @@
-# These are database routines that depend on a specific schema
-# By definition, there exists mixing here with the actual application and its environment
--- backend/src/store/__init__.py
+++
@@ -1,8 +0,0 @@
-from loguru import logger
-from store.error import StoreError
-from store.store import Store, StoreConfig, StoreContext
-
-__all__ = ["Store", "StoreConfig", "StoreContext", "StoreError"]
-
-
-logger.disable("store")
